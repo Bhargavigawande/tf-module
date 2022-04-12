27b15f47f3fa875292d46267fa636cc23972bcf5
Bhargavigawande <95681383+Bhargavigawande@users.noreply.github.com>
2022-04-13 00:36:32 +0530

    Add files via upload

---
+++ c4-02-vpc-module.tf
@@ -0,0 +1,34 @@
+# Create VPC Terraform Module
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.14.0"
+  #version = "~> 3.14"
+
+  # VPC Basic Details
+  name = "${local.name}-${var.vpc_name}"
+  cidr = var.vpc_cidr_block
+  azs             = var.vpc_availability_zones
+  public_subnets  = var.vpc_public_subnets
+  private_subnets = var.vpc_private_subnets  
+
+  
+
+  # VPC DNS Parameters
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+
+  tags = local.common_tags
+  vpc_tags = local.common_tags
+
+  # Additional Tags to Subnets
+  public_subnet_tags = {
+    Type = "Public Subnets"
+  }
+  private_subnet_tags = {
+    Type = "Private Subnets"
+  }  
+  database_subnet_tags = {
+    Type = "Private Database Subnets"
+  }
+}
